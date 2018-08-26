4242045004ee0537330b938628379b3eeff69e06
ampanisales <31353791+ampanisales@users.noreply.github.com>
2018-08-25 17:31:57 -0700

    Update classicCiphers.py

--- classicCiphers.py
+++ classicCiphers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """ classicCiphers.py
-    Able to encrypt or decrypt a text file with a variety of ciphers
+	Able to encrypt or decrypt a text file with a variety of ciphers
+
+	Assignment: Classic Ciphers
 
 __author__ = "Anthony Panisales"
 
 """
 
 import click
-
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 class Cipher(object):
 	""" Base class for the ciphers """
@@ -30,7 +30,7 @@ class CaesarCipher(Cipher):
 	"""
 
 	def getKey(self):
-		""" TODO: Function Description """
+		""" Retrieves the user's desired shift value. """
 		while True:
 			try:
 				key = int(input("Shift value (0-25): "))	
@@ -44,7 +44,21 @@ class CaesarCipher(Cipher):
 				continue
 
 	def encipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Encrypts the file by replacing each character in the
+		    text from the input file with the character a certain
+		    number of places after it in the alphabet. This number
+		    is defined by the user and is the shift value.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be encrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        encrypted text
+
+		"""
 		key = self.getKey()
 		for c in oldFileText:
 			newChar = c
@@ -53,7 +67,21 @@ class CaesarCipher(Cipher):
 			file.write(newChar)
 
 	def decipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Decrypts the file by replacing each character in the
+		    text from the input file with the character a certain
+		    number of places before it in the alphabet. This number
+		    is defined by the user and is the shift value.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be decrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        decrypted text
+
+		"""
 		key = self.getKey()
 		for c in oldFileText:
 			newChar = c
@@ -77,7 +105,7 @@ class VigenereCipher(Cipher):
 	key = []
 
 	def getKey(self):
-		""" TODO: Function Description """
+		""" Retrieves the user's desired keyword. """
 		keyString = ""
 		while not keyString.isalpha():
 			keyString = input("Key: ").upper()	
@@ -88,7 +116,18 @@ class VigenereCipher(Cipher):
 			self.key.append(self.letters.index(c))
 
 	def encipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Encrypts the file using the user's desired keyword.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be encrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        encrypted text
+
+		"""
 		keyIndex = 0
 		self.getKey()
 		for c in oldFileText:
@@ -99,7 +138,18 @@ class VigenereCipher(Cipher):
 			file.write(newChar)
 
 	def decipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Decrypts the file using the user's desired keyword.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be decrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        decrypted text
+
+		"""
 		keyIndex = 0
 		self.getKey()
 		for c in oldFileText:
@@ -134,7 +184,7 @@ class AffineCipher(Cipher):
 	key = []
 
 	def getKey(self):
-		""" TODO: Function Description """
+		""" Retrieves the user's desired values for a and b. """
 		while True:
 			try:
 				a = int(input("a: "))	
@@ -162,7 +212,20 @@ class AffineCipher(Cipher):
 				continue
 
 	def encipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Encrypts the file by performing the Affine cipher's
+		    encryption function using the user's desired values
+		    for a and b.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be encrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        encrypted text
+
+		"""
 		self.getKey()
 		a = self.key[0]
 		b = self.key[1]
@@ -173,7 +236,20 @@ class AffineCipher(Cipher):
 			file.write(newChar)
 
 	def decipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Decrypts the file by performing the Affine cipher's
+		    decryption function using the user's desired values
+		    for a and b.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be decrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        decrypted text
+
+		"""
 		self.getKey()
 		a = self.key[0]
 		b = self.key[1]
@@ -199,7 +275,19 @@ class AtbashCipher(Cipher):
 	"""
 
 	def encipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Encrypts the file by replacing each character in the
+		    text from the input file with its reverse.  
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be encrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        encrypted text
+
+		"""
 		for c in oldFileText:
 			newChar = c
 			if c in self.letters:
@@ -207,7 +295,19 @@ class AtbashCipher(Cipher):
 			file.write(newChar)
 
 	def decipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Decrypts the file by replacing each character in the
+		    text from the input file with its reverse.
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be decrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        decrypted text
+
+		"""
 		self.encipher(oldFileText, file)
 
 
@@ -225,7 +325,7 @@ class SimpleSubstitutionCipher(Cipher):
 	key = []
 
 	def getKey(self):
-		""" TODO: Function Description """
+		""" Retrieves the key/cipher alphabet from the user """
 		keyString = ""
 		while True:
 			keyString = input("Key: ").upper()	
@@ -239,11 +339,25 @@ class SimpleSubstitutionCipher(Cipher):
 				self.key.append(c)
 			if len(self.key) != len(set(self.key)):
 				print("Invalid key: There should be no repeated characters in the key")
+				self.key = []
 				continue
 			break
 
 	def encipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Encrypts the file by replacing each character in the
+		    text from the input file with its equivalent in the
+		    user's desired key/cipher alphabet.  
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be encrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        encrypted text
+
+		"""
 		self.getKey()
 		for c in oldFileText:
 			newChar = c
@@ -252,7 +366,19 @@ class SimpleSubstitutionCipher(Cipher):
 			file.write(newChar)
 
 	def decipher(self, oldFileText, file):
-		""" TODO: Function Description """
+		""" 
+		    Decrypts the file by replacing each character in the
+		    text from the input file with its reverse.  
+
+		    Parameters
+		    ----------
+		    oldFileText : str
+		        The text from the input text file to be decrypted
+		    file : file
+		        The input text file which will be overwritten with
+		        decrypted text
+
+		"""
 		self.getKey()
 		for c in oldFileText:
 			newChar = c
@@ -260,9 +386,15 @@ class SimpleSubstitutionCipher(Cipher):
 				newChar = self.letters[self.key.index(c)]
 			file.write(newChar)
 
+
+# --- click command-line interface code ----------------------------	
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 @click.group(context_settings=CONTEXT_SETTINGS)
 def classicCiphers():
-    pass
+	""" A tool that can encrypt or decrypt a text file with a variety of ciphers """
+	pass
 
 @classicCiphers.command()
 @click.option('-c', is_flag=True, help='use the Caesar Cipher')
@@ -272,7 +404,25 @@ def classicCiphers():
 @click.option('-s', is_flag=True, help='use the Simple Substitution Cipher')
 @click.argument('file', type=click.Path(exists=True))
 def encrypt(c, v, af, at, s, **f):
-	"""Encrypts a file"""
+	""" 
+	    Encrypts a file using one of the available ciphers 
+
+	    Parameters
+	    ----------
+	    c : bool
+	        Boolean flag for the Caesar Cipher. True if the user wants to use it
+	    v : bool
+	        Boolean flag for the Vigenere Cipher. True if the user wants to use it
+	    af : bool
+	        Boolean flag for the Affine Cipher. True if the user wants to use it
+	    at : bool
+	        Boolean flag for the Atbash Cipher. True if the user wants to use it
+	    s : bool
+	        Boolean flag for the Simple Substitution. True if the user wants to use it
+	    f : dict
+	        Dictionary object for the command line arguments that contains the input file
+
+	"""
 	file = open(f.get('file'), 'r')
 	oldFileText = file.read().upper()
 	file.close()
@@ -310,7 +460,25 @@ def encrypt(c, v, af, at, s, **f):
 @click.option('-s', is_flag=True, help='use the Simple Substitution Cipher')
 @click.argument('file', type=click.Path(exists=True))
 def decrypt(c, v, af, at, s, **f):
-	"""Decrypts a file"""
+	""" 
+	    Decrypts a file using one of the available ciphers 
+
+	    Parameters
+	    ----------
+	    c : bool
+	        Boolean flag for the Caesar Cipher. True if the user wants to use it
+	    v : bool
+	        Boolean flag for the Vigenere Cipher. True if the user wants to use it
+	    af : bool
+	        Boolean flag for the Affine Cipher. True if the user wants to use it
+	    at : bool
+	        Boolean flag for the Atbash Cipher. True if the user wants to use it
+	    s : bool
+	        Boolean flag for the Simple Substitution. True if the user wants to use it
+	    f : dict
+	        Dictionary object for the command line arguments that contains the input file
+
+	"""
 	file = open(f.get('file'), 'r')
 	oldFileText = file.read().upper()
 	file.close()
